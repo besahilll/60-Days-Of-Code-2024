4622cbff327f8972d1f39701819b4fa7e29ca720
besahilll <500091837@stu.upes.ac.in>
2024-07-12 23:54:24 +0530

    Find Duplicate number in array

---
+++ Day4/FindDuplicate.java
@@ -0,0 +1,15 @@
+/*Find the duplicate number in array
+Time: O(n)
+Space:O(1)*/
+class Solution {
+    public int findDuplicate(int[] nums) {
+        for(int i=0;i<nums.length;i++){
+            int index= Math.abs(nums[i]);
+            if(nums[index]<0)return index;
+
+            nums[index]=-nums[index];
+        }
+
+        return -1;
+    }
+}
