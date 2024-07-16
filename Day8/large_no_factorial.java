5157b2f4f2e71b82073cc0638ce72d20994bc546
besahilll <500091837@stu.upes.ac.in>
2024-07-17 00:42:37 +0530

    Factorial of a Large number

--- Day8/large_no_factorial.java
+++ Day8/large_no_factorial.java
@@ -2,24 +2,29 @@ class Solution {
     static ArrayList<Integer> factorial(int N) {
         // code here
         ArrayList<Integer> result = new ArrayList<>();
-        result.add(1);
-
-        for (int i = 2; i <= N; i++) {
-            int carry = 0;
-
-            for (int j = 0; j < result.size(); j++) {
-                int val = result.get(j) * i + carry;
-                result.set(j, val % 10);
-                carry = val / 10;
+        result.add(0,1);
+        int size=1;
+        //variable to traverse from 2 to n
+        int val=2,c=0;
+        while(val<=N){
+            //traverse arraylist from right to left
+            for(int i=size-1;i>=0;i--){
+                //updat value in arraylist
+                int temp=result.get(i)*val +c;
+                //store the last digit at index and add remaining to carry
+                result.set(i,temp%10);
+                //update carry
+                c=temp/10;
             }
-
-            while (carry > 0) {
-                result.add(carry % 10);
-                carry /= 10;
+            
+            //insert carry to the beginning of arraylist
+            while(c!=0){
+                result.add(0,c%10);
+                c=c/10;
+                size++;
             }
+            val++;
         }
-
-        Collections.reverse(result);
         return result;
     }
 }
