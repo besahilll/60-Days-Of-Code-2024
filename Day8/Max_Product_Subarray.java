dd09fd635f17b65d2cb613dfbc85f9d031722665
besahilll <500091837@stu.upes.ac.in>
2024-07-17 01:14:28 +0530

    Maximum product subarray

---
+++ Day8/Max_Product_Subarray.java
@@ -0,0 +1,24 @@
+class Solution {
+    // Function to find maximum product subarray
+    long maxProduct(int[] arr, int n) {
+        long max=arr[0];
+        long min=arr[0];
+        long res=arr[0];
+        for(int i=1;i<n;i++){
+            if(arr[i]==0){
+                min=1;
+                max=1;
+                continue;
+            }
+            long temp1=arr[i]*max;
+            long temp2=arr[i]*min;
+            max=Math.max(temp1,temp2);
+            max=Math.max(max,arr[i]);
+            min=Math.min(temp1,temp2);
+            min=Math.min(min,arr[i]);
+            res=Math.max(res,max);
+        }
+        return res;
+        
+    }
+}
