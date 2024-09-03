678b962024d5d14d32d93325851e1ce41904ba24
besahilll <500091837@stu.upes.ac.in>
2024-09-03 14:26:35 +0530

    Right view of tree

---
+++ Day51/right_view_of_tree.java
@@ -0,0 +1,47 @@
+ /*Complete The Function Provided
+Given Below is The Node Of Tree
+class Node
+{
+    int data;
+    Node left, right;
+    public Node(int data)
+    {
+        this.data = data;
+         left = right = null;
+    }
+}*/
+
+class Solution {
+    // Function to return list containing elements of right view of binary tree.
+    ArrayList<Integer> rightView(Node root) {
+        ArrayList<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int n = queue.size();  
+
+            for (int i = 1; i <= n; i++) {
+                Node currentNode = queue.poll();
+
+                if (i == n) {
+                    result.add(currentNode.data);
+                }
+
+                if (currentNode.left != null) {
+                    queue.add(currentNode.left);
+                }
+
+                if (currentNode.right != null) {
+                    queue.add(currentNode.right);
+                }
+            }
+        }
+
+        return result;
+    }
+}
